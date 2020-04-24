e3c39ff39f4251cf8cbc8b0031840bb5cb2f6d9a
5 Second #Skylark <59589591+skylark17@users.noreply.github.com>
2020-04-24 09:34:39 +0700

    Sonic

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=STRIX Kernel by fiqri19102002 @ github
+kernel.string=Sonic Kernel by SkylarkAurora @ github
 do.devicecheck=1
 do.modules=0
 do.systemless=1
